c98c676aa1542de6aaf7596706c4978679d16520
Iverson.hu <mehudeyi@163.com>
2019-10-11 10:05:36 +0800

    maptalks.formats

---
+++ src/maptalks.formats/test/test.js
@@ -0,0 +1,111 @@
+describe('maptalks.formats', function () {
+
+    it('gpx', function (done) {
+        maptalks.Formats.gpx('/base/test/a.gpx', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+
+    it('csv fail', function (done) {
+        maptalks.Formats.csv('/base/test/a.gpx', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+
+    it('csv options', function (done) {
+        maptalks.Formats.csv('/base/test/options.csv', {
+            latfield: 'a',
+            lonfield: 'b'
+        }, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+
+    it('kml', function (done) {
+        maptalks.Formats.kml('/base/test/a.kml', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+
+
+    it('csv', function (done) {
+        maptalks.Formats.csv('/base/test/a.csv', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+
+    it('polyline', function (done) {
+        maptalks.Formats.polyline('/base/test/a.polyline', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+
+    it('csv.parse', function (done) {
+        maptalks.Formats.csv.parse('lat,lon,title\n0,0,"Hello"', null, function (err, geojson) {
+            if (err) {
+                throw err;
+            }
+            expect(geojson).to.be.ok();
+            done();
+        });
+    });
+
+    it('wkt.parse', function () {
+        var geojson = maptalks.Formats.wkt.parse('MultiPoint(20 20, 10 10, 30 30)');
+        expect(geojson).to.be.ok();
+    });
+
+    it('wkt', function (done) {
+        maptalks.Formats.wkt('/base/test/a.wkt', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+
+    it('topojson', function (done) {
+        maptalks.Formats.topojson('/base/test/a.topojson', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+
+    it('geojson', function (done) {
+        maptalks.Formats.geojson('/base/test/a.geojson', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+});
